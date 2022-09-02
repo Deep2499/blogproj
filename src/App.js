52aff46ac3fd64aa2a64830d7d8c0ae6a6838ba3
Deep2499 <106651757+Deep2499@users.noreply.github.com>
2022-09-02 18:03:53 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import { AllPosts, OnePost} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>   
+        <Route component={AllPosts} path="/" exact />
+        <Route component={OnePost} path="/:slug" exact />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+
